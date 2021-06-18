d0fd83aedd377391a3ea08211dceb0163509d19f
Amartya Chakraborty <amartyachakraborty@Amartyas-MacBook-Pro-2.local>
2021-06-18 13:39:23 -0400

    Cleanup directory

--- quickstart_files/install_opni_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget https://github.com/rancher/opni-docs/raw/main/quickstart_files/opnictl
-chmod +x opnictl
-./opnictl install --quickstart
--- quickstart_files/install_opni_windows.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://github.com/rancher/opni-docs/raw/main/quickstart_files/opnictl-windows
-./opnictl-windows install --quickstart
