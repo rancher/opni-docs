80024c7051d41feedc27d2875cbbe63938a38c26
Amartya Chakraborty <amartyachakraborty@Amartyas-MacBook-Pro-2.local>
2021-06-18 14:05:56 -0400

    Update installation script with correct path

--- quickstart_files/install_opni.sh
+++ quickstart_files/install_opni.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget https://github.com/rancher/opni-docs/raw/demo/quickstart_files/opnictl
 chmod +x opnictl
-wget https://github.com/rancher/opni-docs/raw/demo/quickstart_files/quickstart_demo.sh 
+wget https://raw.githubusercontent.com/rancher/opni-docs/demo/quickstart_files/quickstart_demo.sh
 chmod +x quickstart_demo.sh
 ./quickstart_demo.sh
