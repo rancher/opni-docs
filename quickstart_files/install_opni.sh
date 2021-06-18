c17847bdf52d0832d28b4f84045d666a637e1f72
Amartya Chakraborty <amartyachakraborty@Amartyas-MacBook-Pro-2.local>
2021-06-18 14:49:58 -0400

    Make sure opnictl file is placed in correct path

--- quickstart_files/install_opni.sh
+++ quickstart_files/install_opni.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+sudo su
 wget https://github.com/rancher/opni-docs/raw/demo/quickstart_files/opnictl
 chmod +x opnictl
 wget https://raw.githubusercontent.com/rancher/opni-docs/demo/quickstart_files/quickstart_demo.sh
 chmod +x quickstart_demo.sh
+mv quickstart_demo.sh /usr/local/bin
 ./quickstart_demo.sh
