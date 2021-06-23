3198068bcc144c0c9ee471912fdb2c2bfc663a68
Amartya Chakraborty <amartyachakraborty@Amartyas-MBP-2.fios-router.home>
2021-06-22 20:46:30 -0400

    Add labels to jobs and add option to delete jobs if user chooses to do so

--- quickstart_files/errors_injection.sh
+++ quickstart_files/errors_injection.sh
@@ -6,6 +6,8 @@ inject_unschedulable_pods_anomaly() {
     kind: Job
     metadata:
       name: test-job-unschedulable
+      labels:
+        is-opni-demo: "true"
     spec:
       completions: 10
       parallelism: 10
@@ -33,6 +35,8 @@ inject_nonexistent_image_pods_anomaly() {
     kind: Job
     metadata:
       name: test-job-nonexistent-image
+      labels:
+        is-opni-demo: "true"
     spec:
       completions: 10
       parallelism: 10
@@ -54,6 +58,8 @@ inject_nonzero_exit_code_pods_anomaly() {
     kind: Job
     metadata:
       name: test-job-nonzero-exit-code
+      labels:
+        is-opni-demo: "true"
     spec:
       completions: 10
       parallelism: 10
@@ -68,14 +74,19 @@ inject_nonzero_exit_code_pods_anomaly() {
 EOF
 }
 
+undo_anomalies() {
+  KUBECONFIG="${KUBECONFIG:-/etc/rancher/rke2/rke2.yaml}" PATH="${PATH:+${PATH}:}/var/lib/rancher/rke2/bin/" kubectl delete jobs -l is-opni-demo=true
+}
+
 get_user_anomaly_input() {
   while true
   do
   	echo " Possible Anomalies to inject"
   	echo "1) Create 10 unschedulable pods."
   	echo "2) Create 10 pods with nonexistent images."
-  	echo "3) Create 10 pods that will exit with non-zero exit codes"
-    echo "q) Exit this script"
+  	echo "3) Create 10 pods that will exit with non-zero exit codes."
+    echo "u) Undo anomalies injected into cluster."
+    echo "q) Exit this script."
   	read -p "Type the number of the anomaly you would like to inject: " anomaly
   	if [ "$anomaly" = '1' ]; then
   		echo "Injecting the fault to create 10 unschedulable pods."
@@ -86,6 +97,9 @@ get_user_anomaly_input() {
   	elif [ "$anomaly" = '3' ]; then
   		echo "Injecting the fault to create 10 pods that will exit with non-zero exit codes."
   		inject_nonzero_exit_code_pods_anomaly
+    elif [ "$anomaly" = 'u' ]; then
+      echo "Undoing anomalies injected into systems."
+      undo_anomalies
     elif [ "$anomaly" = 'q' ]; then
       break
   	fi
